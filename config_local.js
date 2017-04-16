e41e0b620ec1afdc2ad5dcf2a9f4e54c2c0e3903
Praveen Gangasani <tenpowerofnine@gmail.com>
2017-04-16 08:14:04 -0500

    Initial Commit

---
+++ config_local.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  mailer : {
+    service : '', // Email service type like 'Gmail'
+    auth : {
+      user : '', // Enter your email a/c
+      pass : '' // Enter your password
+    }
+  },
+  dbMongoConnection : '' // MongoDB connection URL
+}
---
+++ routes/index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+
+var nodemailer = require('nodemailer');
+var config = require('../config');
+var transporter = nodemailer.createTransport(config.mailer);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Programming made easy'
+ });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {title : 'PairProgramming: Programming made easy'});
+});
+
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', {title : 'PairProgramming: Programming made easy'});
+  })
+  .post(function(req, res, next) {
+
+    req.checkBody('name', 'Empty Name').notEmpty();
+    req.checkBody('email', 'Invalid Email').isEmail();
+    req.checkBody('message', 'Empty Message').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors) {
+      res.render('contact', {
+        title : 'PairProgramming: Programming made easy',
+        name : req.body.name,
+        email : req.body.email,
+        message : req.body.message,
+        errorMessages : errors
+      });
+    }
+    else {
+
+      var mailOptions = {
+        from : 'noreply@gmail.com',
+        to : 'bluemix1729@gmail.com',
+        subject : 'You got a new message from PairProgramming 👍',
+        text : req.body.message
+      }
+
+      transporter.sendMail(mailOptions, function(error, info) {
+        if(error) {
+          console.log(error);
+        }
+        res.render('thanks', {title : 'PairProgramming: Programming made easy'});
+      });
+    }
+  });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title : 'Login'})
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', {title : 'Register'})
+});
+
+module.exports = router;
